574d9913ed61f073ccfb2594614af42c47c7c9cb
Jannis Aziz K <aziz_khammas@yahoo.de>
2022-03-25 20:28:11 +0100

    Updated app.py, changed GET & POST for movies.sqlite DB

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify
+import json
+import sqlite3
+
 app = Flask(__name__)
 
 
-movies_list = [
-    {"id": 0,
-     "title": "Spiderman"},
-    {"id": 1,
-     "title": "Batman"},
-    {"id": 2,
-     "title": "Superman"},
-    {"id": 3,
-     "title": "Aquaman"}
-]
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("movies.sqlite")
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 
 @app.route("/")
@@ -21,45 +21,24 @@ def home():
 
 @app.route("/movies", methods=['GET', 'POST'])
 def movies():
+    conn = db_connection()
+    cursor = conn.cursor()
+
     if request.method == 'GET':
-        if len(movies_list) > 0:
-            return jsonify(movies_list)
-        else:
-            "Nothing found", 404
+        cursor = conn.execute("SELECT * FROM movie")
+        movies = [
+            dict(id=row[0], title=row[1])
+            for row in cursor.fetchall()
+        ]
+        if movies is not None:
+            return jsonify(movies)
 
     if request.method == 'POST':
         new_title = request.form['title']
-        iD = movies_list[-1]['id'] + 1
-
-        new_movie = {
-            'id': iD,
-            'title': new_title
-        }
-
-        movies_list.append(new_movie)
-        return jsonify(movies_list), 201
-
-
-@app.route("/movie/<int:id>", methods=['GET', 'PUT', 'DELETE'])
-def single_movie(id):
-    if request.method == 'GET':
-        for movie in movies_list:
-            if movie['id'] == id:
-                return jsonify(movie)
-    if request.method == 'PUT':
-        for movie in movies_list:
-            if movie['id'] == id:
-                movie['title'] = request.form['title']
-                updated_movie = {
-                    'id': id,
-                    'title': movie['title']
-                }
-                return jsonify(updated_movie)
-    if request.method == 'DELETE':
-        for index, movie in enumerate(movies_list):
-            if movie['id'] == id:
-                movies_list.pop(index)
-                return jsonify(movies_list)
+        sql = """INSERT INTO movie (title) VALUES (?)"""
+        cursor = cursor.execute(sql, [new_title])
+        conn.commit()
+        return f"Movie with the id: {cursor.lastrowid} created successfully", 201
 
 
 if __name__ == '__main__':
