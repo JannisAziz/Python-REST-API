a0c9bff4ba18c5b520b47775963b9c0de99701ec
Jannis Aziz K <aziz_khammas@yahoo.de>
2022-03-25 19:41:38 +0100

    Added db.py, implemented sqlite db with 'movie' table

---
+++ db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect("movies.sqlite")
+
+cursor = conn.cursor()
+sql_query = """ CREATE TABLE movie (
+	id integer PRIMARY KEY,
+	title text NOT NULL
+)"""
+cursor.execute(sql_query)
